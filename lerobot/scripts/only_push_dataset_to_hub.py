653e351aec1d382f007c82d570c1856f416c38f1
Ville Kuosmanen <ville@villekuosmanen.com>
2025-04-20 14:03:14 +0100

    feat: add finetuning analysis scripts (#9)
    * feat: add finetuning analysis scripts
    
    * changes
    
    * changes to main
    
    * fix
    
    * changes

--- lerobot/scripts/only_push_dataset_to_hub.py
+++ lerobot/scripts/only_push_dataset_to_hub.py
@@ -4,4 +4,4 @@ dataset = LeRobotDataset(
     "villekuosmanen/dAgger_pack_easter_eggs_into_basket_2.0.1",
     # root='data/villekuosmanen/pack_easter_eggs_into_basket',
 )
-dataset.push_to_hub()
+dataset.push_to_hub(private=True)
